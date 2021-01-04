5929a3858a1588fd12cc6b05c10165f7a9cbd1ea
shenqiuzhuyun <shenqiuzhuyun@163.com>
2021-01-04 15:08:45 +0800

    'first'

---
+++ passjava-channel/src/test/java/com/lyc/passjava/passjavachannel/PassjavaChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyc.passjava.passjavachannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
